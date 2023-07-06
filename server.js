347cfef118831e3c22d13258cbc9360eaa5618e6
ariannacerboncini <116767931+ariannacerboncini@users.noreply.github.com>
2023-07-06 16:34:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -33,12 +33,58 @@ app.post('/api/authenticate', async (req, res) => {
     const user = users.find(user => user.username === username);
     
     if (user && await bcrypt.compare(password, user.password)) {
-        const token = jwt.sign({ name: user.name }, SECRET_KEY, {
+        const token = jwt.sign({ name: user.name, username: user.username }, SECRET_KEY, {
         expiresIn: "1h",
     });
         res.json({ token });
     } else {
         res.status(401).json({ message: 'Invalid username or password' });
+    }
+});
+
+app.post('/api/updateProfile', async (req, res) => {
+    const token = req.headers['authorization'];
+    const { name } = req.body;
+
+    try {
+        const decoded = jwt.verify(token, SECRET_KEY);
+        const user = users.find(user => user.name === decoded.name);
+
+        if (user) {
+            user.name = name;
+            res.json({ message: 'Profile updated' });
+        } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    } catch (error) {
+        res.status(401).json({ message: 'Invalid token' });
+    }
+});
+
+app.post('/api/updateDetails', async (req, res) => {
+    const token = req.headers['authorization'];
+    const { bio, projects, skills, contacts } = req.body;
+    
+    try {        
+        console.log('Token:', token);
+        const decoded = jwt.verify(token, SECRET_KEY);
+        console.log('Decoded:', decoded);
+        const user = users.find(user => user.username === decoded.username);
+        console.log(user.username);
+        console.log(decoded.username);
+
+        if (user) {
+            user.bio = bio;
+            user.projects = projects;
+            user.skills = skills;
+            user.contacts = contacts;
+
+            res.json({ message: 'Details updated' });
+        } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    } catch (error) {
+        res.status(401).json({ message: 'Invalid token' });
     }
 });
 
